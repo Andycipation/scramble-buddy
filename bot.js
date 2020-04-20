fa055e2934df80a9294e55cc1dec3e1fdd97310a
AndrewDongAndy <dongandrew99@gmail.com>
2020-04-20 16:06:38 -0400

    add scramble logic

--- bot.js
+++ bot.js
@@ -6,39 +6,71 @@ https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
   colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
   token: auth.token,
   autorun: true
 });
+
 bot.on('ready', function(evt) {
-  logger.info('Connected');
+  logger.info('ScrambleBot connected');
   logger.info('Logged in as: ');
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function(user, userID, channelID, message, evt) {
+
+// ==========SCRAMBLE LOGIC==========
+
+function randInt(lo, hi) {
+  return lo + Math.floor(Math.random() * (hi - lo + 1));
+}
+
+const SIDES = ['U', 'D', 'L', 'R', 'F', 'B'];
+let ok = Array(6);
+const DIR = ['', "'", '2'];
+
+function getScramble(moves) {
+  let last = -1;
+  let res = [];
+  for (let i = 0; i < SIDES.length; i++) {
+    ok[i] = true;
+  }
+  for (let i = 0; i < moves; i++) {
+    let x;
+    do {
+      x = randInt(0, SIDES.length - 1);
+    } while (!ok[x]);
+    ok[x] = false;
+    for (let j = 0; j < SIDES.length; j++) {
+      if (j != x && j != (x ^ 1)) {
+        ok[j] = true;
+      }
+    }
+    res.push(SIDES[x] + DIR[randInt(0, 2)]);
+  }
+  return res.join(' ');
+}
+
+// ==========END SCRAMBLE LOGIC==========
+
+bot.on('message', function(user, userId, channelId, msg, evt) {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
-
-    args = args.splice(1);
-    switch(cmd) {
-      // !ping
-      case 'ping':
-      bot.sendMessage({
-        to: channelID,
-        message: 'Pong!'
-      });
-      break;
-      // Just add any case commands if you want to..
-    }
+  if (msg.charAt(0) != '!') {
+    return;
+  }
+  msg = msg.substring(1);
+  if (msg.charAt(0) == '3') {
+    bot.sendMessage({
+      to: channelId,
+      message: getScramble(20)
+    });
   }
 });
